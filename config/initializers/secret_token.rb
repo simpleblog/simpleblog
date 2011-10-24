aabbbc8baf56ed2c0c7daed238613b5dda15626a
Steven Shen <steven_shen1020@foxmail.com>
2011-10-24 16:01:51 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleblog::Application.config.secret_token = '20bfdf4d2bc846f254a4f5e9d24bf893b670df6c8a8a8a330df878ecf5fb274a425942a624ea3076b26d2120284f3c2d4e9ace5d72bd45427fc084f87cdb4254'
