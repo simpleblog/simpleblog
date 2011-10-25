13b9ff4ce42810257a25d75234b5caaec437b2f7
Steven Shen <steven_shen1020@foxmail.com>
2011-10-25 17:32:09 +0800

    add_feature

--- features/step_definitions/add_comment_steps.rb
+++ features/step_definitions/add_comment_steps.rb
@@ -1,6 +1,6 @@
 Given /^I have a comment with title "([^"]*)"$/ do |title|
-  blog=Blog.new
-  blog.title=title
+  blog=Blog.new;
+  blog.title=title;
   blog.save
 end
 
@@ -8,12 +8,16 @@ Then /^I go to the blogs page$/ do
   visit '/blogs'
 end
 
-Then /^I click on "([^"]*)"$/ do |title|
+Then /^I click on "([^"]*)" link$/ do |title|
   click_link(:title)
 end
 
 Then /^I should see "([^"]*)"$/ do |arg1|
-
+    if page.respond_to? :should
+       page.should have_content(arg1)
+    else
+       assert page.has_content?(arg1)
+    end
 end
 
 Given /^I have a comment with content "([^"]*)"$/ do |content|
@@ -22,6 +26,15 @@ Given /^I have a comment with content "([^"]*)"$/ do |content|
   comment.save
 end
 
-When /^I fill in (.*) with "([^"]*)"$/ do |content,value|
+When /^I fill in ([^"]*) with "([^"]*)"$/ do |content,value|
   fill_in(content,:with=>value)
+end
+
+When /^I click on "([^"]*)" button$/ do |locator|
+    begin
+        click_on(locator)
+    rescue Capybara::ElementNotFound
+        # if locator isn't found in a link or button, try looking for submit buttons with value=locator
+        find("input[type='submit'][value='#{locator}']")
+    end
 end
