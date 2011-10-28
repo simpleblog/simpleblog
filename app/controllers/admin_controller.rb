faa7933f892c87c2cde7369297c8df1e98e461bd
alienyu <alienyu@foxmail.com>
2011-10-28 09:25:24 +0800

    1. add session in admin 2. add_blog edit 3. optimize cucumber

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,11 +4,9 @@ class AdminController < ApplicationController
   end
 
   def login
-    #  if request.post?
     @username = params[:username]
     @password = params[:password]
     user = User.find_by_username_and_password(params[:username],params[:password])
-    #@pass = user.password
     if user
       redirect_to :action => 'add_blogs'
     else
@@ -23,7 +21,7 @@ class AdminController < ApplicationController
   end
 
   def add_blogs
-    @blog = Blog.find(:all)
+    @blogs = Blog.find(:all)
     @new_blog = Blog.new
   end
 
@@ -33,6 +31,20 @@ class AdminController < ApplicationController
     if @new_blog.save
       flash[:notice] = @new_blog.title
       redirect_to :controller => 'admin',:action => 'add_blogs',:id => @id
+    end
+  end
+
+  def edit
+    @id = params[:id]
+    @edit_blog = Blog.find(@id)
+    @blogs = Blog.find(:all)
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update_attributes(params[:blog])
+      flash[:notice] = "Edit Success!"
+      redirect_to :controller => "admin" ,:action => "add_blogs"
     end
   end
 end
--- features/add_blog.feature
+++ features/add_blog.feature
@@ -1,9 +1,9 @@
 Feature: user add blog
   Scenario:  Admin adds blog to the site
-    Given I write a blog with title "steven" and content "2b"
+    Given I write a blog with title "allen" and content "22222"
     And I go to the add_blogs page
-    When I fill in blog_title with "steven"
-    And I fill in blog_content with "sb"
+    When I fill in blog_title with "allen"
+    And I fill in blog_content with "22222"
     And I click on "CREATE" button
     Then I should see "steven"
 
